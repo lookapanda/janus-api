d839068959c79f8f15598616a27088a5caaf02db
Simon <sn@hitbox.tv>
2019-03-08 14:50:57 +0100

    TS Test 2

---
+++ lib/index.d.ts
@@ -0,0 +1,7 @@
+export * from './Janus';
+export * from './JanusPlugin';
+export * from './SdpHelper';
+export * from './shared';
+export * from './WebSocket';
+export * from './Config';
+export * from './plugin/StreamingJanusPlugin';
